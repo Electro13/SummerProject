44d41abd82af15aa1ae21812524b8437e075435e
KeirnanO <45085114+KeirnanO@users.noreply.github.com>
2021-06-29 16:59:26 -0400

    ADDED_SimpleCharacterController_InventorySystem_Interactables
    3rd person character controller with a semi working Camera controller
    * mostly for testing purposes will be more refined as I work on it
    * Unity was broke. So I built this from scratch - needs work
    
    Imported an inventory system/ itemdatabase
    * Allows easy creation of items by adding them to the item text file
    * Inventory slots are swappable
    
    Implemented Interactables
    * So far you can interact with a pickup item and add it to your inventory

---
+++ SGP/Assets/Scripts/Controllers/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform center;
+    public MouseLook look;
+    public Vector3 truePosition;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //If there is a wall inbetween our camera and player, move camera to the wall
+
+        RaycastHit hit;//temp
+        
+        if (Physics.Raycast(center.position, transform.position - center.position, out hit, (transform.position - center.position).magnitude))
+        {
+            Vector3 forward = -(transform.position - center.position).normalized;
+            transform.position = hit.point + forward;
+            Debug.DrawRay(center.position, hit.point - center.position);
+        }
+        else
+        {
+            Debug.DrawRay(center.position, transform.position - center.position);
+            transform.localPosition = truePosition;
+        }
+    }
+
+}
